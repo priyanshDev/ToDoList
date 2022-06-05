8291e8efdaedcf8e9b0fc1df11f0cc23ecc632c3
Priyansh <priyanshkumar205@gmail.com>
2022-06-05 23:55:38 +0530

    Added Initial Files

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay === 6){
+        day = "Saturday";
+    } 
+    else if(currentDay === 0) {
+        day = "Sunday";
+    } 
+    else if(currentDay === 1) {
+        day = "Monday";
+    } 
+    else if (currentDay === 2) {
+        day = "Tuesday";
+    } 
+    else if (currentDay === 3) {
+        day = "Wednesday";
+    } 
+    else if (currentDay === 4) {
+        day = "Thrusday";
+    } 
+    else {
+        day = "Friday";
+    }
+
+    res.render("list", {kindOfDay: day});
+    
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+});
