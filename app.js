36d453604d00e276a909dc2b166ea9bf380c5575
Priyansh <priyanshkumar205@gmail.com>
2022-06-07 23:55:22 +0530

    updated site

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items =["Buy Food", "Cook Food", "Eat Food"];
+let items =["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
@@ -11,21 +11,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {kindOfDay: day, newListItems: items});
     
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 })
